663e46a37416c8630bcedaa3bde2134ed17e972d
Mario <mariobielborba@hotmail.com>
2024-10-09 21:48:10 -0300

    Ajustado Fragment, e adicionado props ao componente Card

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -1,10 +1,15 @@
-import Card from "./components/Card/"
+import Button from "./components/Button";
+import Card from "./components/Card/";
+import swPosterImg from "./assets/poster.jpg";
+import esbPosterImg from "./assets/esb.jpg";
+import jediPosterImg from "./assets/jedi.jpg";
 
 export default function App() {
-
   return (
-    <div className="app">
-      <Card/>
-    </div>
-  )
+    <>
+      <Card title="Poster: Star Wars(1977)" posterImg={swPosterImg} />
+      <Card title="Poster: Empire Strikes Back (1980)" posterImg={esbPosterImg}/>
+      <Card title="Poster: Return of the Jedi (1983)"  posterImg={jediPosterImg}/>
+    </>
+  );
 }
--- card/src/components/Card/index.jsx
+++ card/src/components/Card/index.jsx
@@ -1,15 +1,15 @@
-import posterImg from "../../assets/poster.jpg"
+import Button from "../Button"
 import styles from "./styles.module.css"
 
-export default function Card() {
+export default function Card({title, posterImg}) {
 
   return(
     <div className={styles.container}>
-      <img className={styles.poster} src={posterImg} alt="Star Wars poster" />
+      <img className={styles.poster} src={posterImg} alt={title} />
       <div>
-        <h2>Pôster: Star Wars (1977)</h2>
+        <h2>{title}</h2>
         <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
-        <button>Comprar agora</button>
+        <Button/>
       </div>
     </div>
   )
